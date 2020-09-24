298b856ec009bfb190c297c67a5bceb210e3bd7e
DESKTOP-A4FEECK\水怪 <826205363@>
2020-09-24 10:40:35 +0800

    模拟抢购

--- service/api/api_test_service.py
+++ service/api/api_test_service.py
@@ -6,17 +6,39 @@ def api_test_service(data):
     redis = Redis_driver()
 
     lock = redis_lock.Lock(redis.conn, "test-lock")
-    if lock.acquire(blocking=True,timeout=30):
+    # # 悲观锁
+    # if lock.acquire(blocking=True,timeout=30):
+    #     val = redis.get("test")
+    #     if val is None:
+    #         val = 0
+    #     val = int(val)
+    #     if val >= 10:
+    #         lock.release()
+    #         return True, "sold out"
+    #     redis.set("test", value=str(val+1), expire=120)
+    #     lock.release()
+    #
+    #     return True, "ok"
+    # else:
+    #     return False, "time out"
+
+    # 乐观锁 在并发抢购的模式中，乐观锁确实优秀些
+    val = redis.get("test")
+    if val is None:
+        val = 0
+    val = int(val)
+    if val >= 10:
+        return True, "sold out case 1"
+    if lock.acquire(blocking=True, timeout=30):
         val = redis.get("test")
         if val is None:
             val = 0
         val = int(val)
-        if val >= 10:
+        if val < 10:
+            redis.set("test", value=str(val+1), expire=120)
+        else:
             lock.release()
-            return True, "sold out"
-        redis.set("test", value=str(val+1), expire=120)
+            return True, "sold out case 2"
         lock.release()
 
-        return True, "ok"
-    else:
-        return False, "time out"
+    return True, "ok"
--- test_tools/test.py
+++ test_tools/test.py
@@ -5,6 +5,6 @@ def send(num):
     res = requests.get("http://localhost:8000/api/test?name=%s" % num)
     print(num, res.text)
 
-for i in range(20):
+for i in range(40):
     t = threading.Thread(target=send, args=(i,))
     t.start()
