4143f3cfa6e033bd338198d634d514e3c0e64a53
DESKTOP-A4FEECK\水怪 <826205363@>
2021-01-07 11:43:08 +0800

    修正db_driver内部错误

--- db_driver/mysql_driver.py
+++ db_driver/mysql_driver.py
@@ -1,7 +1,6 @@
 import pymysql
-from DBUtils.PooledDB import PooledDB
-from config.constans import MAXCONNECTIONS, MINCACHED, MAXCACHED, MAXSHARED, BLOCKING, MAXUSAGE, SETSESSION, PING, \
-    MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_CHARSET
+from dbutils.pooled_db import PooledDB
+from config.constans import *
 from exception.BaseExpection import Base_Exception
 
 
@@ -32,7 +31,7 @@ class Mysql_Driver:
             cls._instance = object.__new__(cls)
         return cls._instance
 
-    def connect(self):
+    def _connect(self):
         conn = self.POOL.connection()
         cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
         return conn, cursor
@@ -43,26 +42,50 @@ class Mysql_Driver:
         conn.close()
 
     def fetchone(self, sql):
-        conn, cursor = self.connect()
+        conn, cursor = self._connect()
         cursor.execute(sql)
         result = cursor.fetchone()
         self.connect_close(conn, cursor)
         return result
 
     def fetchall(self, sql):
-        conn, cursor = self.connect()
+        conn, cursor = self._connect()
         cursor.execute(sql)
         result = cursor.fetchall()
         self.connect_close(conn, cursor)
         return result
 
-    def execute(self, sql):
-        conn, cursor = self.connect()
-        cursor.execute(sql)
+    def execute_one(self, sql):
+        conn, cursor = self._connect()
         try:
-            cursor.commit()
+            cursor.execute_one(sql)
+            conn.commit()
         except Base_Exception:
             conn.rollback()
+            self.connect_close(conn, cursor)
             return False
         self.connect_close(conn, cursor)
         return True
+
+    def begin(self):
+        """
+        事务开始
+        实际上pymysql一直都是autocommit(0)，所以写个begin在代码层面上没有意义
+        但在结构层面可以标志这是一个事务，便于代码理解
+        :return: conn, cursor 使用begin开始的事务，使用cursor.execute()执行sql
+        """
+        conn, cursor = self._connect()
+        return conn, cursor
+
+    def end(self, conn, cursor):
+        """
+        事务结束
+        :return:
+        """
+        try:
+            conn.commit()
+        except Exception:
+            conn.rollback()
+            self.connect_close(conn, cursor)
+
+db = Mysql_Driver()
